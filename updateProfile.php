bb35d0ce80b7be5a8c2efa6554d7c5853047cd31
seannich <seannts9@gmail.com>
2021-01-28 22:39:32 +0800

    update profile function completed

--- updateProfile.php
+++ updateProfile.php
@@ -1,20 +1,43 @@
 <script type="text/javascript">
-function validateForm()
-{
+function validateForm() {
     // Check if password matched
-	if (document.changePwd.pwd1.value != document.changePwd.pwd2.value) {
- 	    alert("Passwords not matched!");
-        return false;   // cancel submission
+    if (document.changePwd.pwd1.value != document.changePwd.pwd2.value) {
+        alert("Passwords not matched!");
+        return false; // cancel submission
     }
-    return true;  // No error found
+
+    if (document.register.phone.value != ""){
+        var str = document.register.phone.value;
+        if (str.length != 8){
+            alert("Please enter a 8-digit phone number.");
+            return false; //cancel submission
+        }
+        else if (str.substr(0,1) != "6" &&
+                 str.substr(0,1) != "8" &&
+                 str.substr(0,1) != "9") {
+            alert("Phone number in Singapore should start with 6,8 or 9.");
+            return false; //cancel submission
+        }
+    }
+    return true; // No error found
 }
 </script>
 
 <?php
 // Detect the current session
 session_start();
-// To Do 1: Check if user logged in 
 
+$shopperID = $_SESSION["ShopperID"];
+
+// Retrieve shopper record based on e-mail address
+include_once("mysql_conn.php");
+$qry = "SELECT * FROM Shopper WHERE ShopperID = ?" ;
+$stmt = $conn->prepare($qry);
+$stmt->bind_param("s", $shopperID); 	// "s" - string 
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+$row = $result->fetch_array();
 // End of To Do 1
 
 $MainContent = "<h1 class='page-title' style='text-align:center;'>Update Profile</h1>";
@@ -29,8 +52,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='name' style='text-align:right;'>*Name:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='name' id='name' 
-                  type='text' required /> (required)";
+$MainContent .= "<input class='form-control' name='name' id='name' type='text' value='$row[Name]' required />";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -38,7 +60,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='name' style='text-align:right;'>Date of Birth:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='birthdate' id='birthdate' type='date' />";
+$MainContent .= "<input class='form-control' name='birthdate' id='birthdate' value='$row[BirthDate]' type='date' />";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -46,7 +68,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='address' style='text-align:right;'>Address:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<textarea class='form-control' name='address' id='address' cols='25' rows='4'></textarea>";
+$MainContent .= "<textarea class='form-control' name='address' id='address' cols='25' rows='4' value='$row[Address]'></textarea>";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -54,7 +76,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='country' style='text-align:right;'>Country:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='country' id='country' type='text' />";
+$MainContent .= "<input class='form-control' name='country' id='country' type='text' value='$row[Country]' />";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -62,7 +84,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='phone' style='text-align:right;'>Phone:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='phone' id='phone' type='text' />";
+$MainContent .= "<input class='form-control' name='phone' id='phone' type='text' value='$row[Phone]' />";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -70,7 +92,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='email' style='text-align:right;'>*Email Address:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='email' id='email' type='email' required /> (required)";
+$MainContent .= "<input class='form-control' name='email' id='email' type='email' value='$row[Email]' required />";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -94,7 +116,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='pwdQn' style='text-align:right;'>*Security Question:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<select class='form-control' name='pwdQn' id='pwdQn' type='text' required> 
+$MainContent .= "<select class='form-control' name='pwdQn' id='pwdQn' type='text' value='$row[PwdQuestion]' required> 
                 <option>Which polytechnic did you graduate from?</option>
                 <option>How many siblings do you have?</option>
                 <option>What is your middle name?</option>
@@ -107,7 +129,7 @@ $MainContent .= "</div>";
 $MainContent .= "<div class='form-group row'>";
 $MainContent .= "<label class='col-sm-3 col-form-label' for='pwdAns' style='text-align:right;'>*Answer:</label>";
 $MainContent .= "<div class='col-sm-9'>";
-$MainContent .= "<input class='form-control' name='pwdAns' id='pwdAns' type='text' required/>";
+$MainContent .= "<input class='form-control' name='pwdAns' id='pwdAns' type='text' value='$row[PwdAnswer]' required/>";
 $MainContent .= "</div>";
 $MainContent .= "</div>";
 
@@ -122,19 +144,31 @@ $MainContent .= "</div>";
 $MainContent .= "</div>";
 
 $MainContent .= "</form>";
+$MainContent .= "</div>";
 
-// Process after user click the submit button
-if (isset($_POST['pwd1'])) {
-	// To Do 2: Read new password entered by user
-	
-	
-	// To Do 3: Hash the default password
-	
-	
-	// To Do 4: Update the new password hash
-	
+// Check if there is a Name
+if (isset($_POST['name']) || isset($_POST['birthdate']) || isset($_POST['address']) || isset($_POST['country']) || isset($_POST['phone']) || isset($_POST['email']) || isset($_POST['pwdQn']) || isset($_POST['pwdAns'])) {
+
+    if ($_POST['email'] == $row['Email']) {
+        echo '<script>alert("Email already in use!")</script>';
+        header('updateProfile.php'); 
+    } else {
+        if (isset($_POST['pwd1']) && isset($_POST['pwd2']) && $_POST['pwd1'] === $_POST['pwd2']) {
+    
+            $qry = "UPDATE shopper SET Password = ? WHERE ShopperID = ?";
+            $stmt = $conn->prepare($qry);
+            $stmt->bind_param("si", $_POST['pwd1'], $_SESSION["ShopperID"]);
+            $stmt->execute();
+            $stmt->close();
+        }
+        $qry = "UPDATE shopper SET Name = ?, BirthDate = ?, Address = ?, Country = ?, Phone = ?, Email = ?, PwdQuestion = ?, PwdAnswer = ? WHERE ShopperID = ?";
+        $stmt = $conn->prepare($qry);
+        $stmt->bind_param("sssssssss", $_POST['name'], $_POST['birthdate'], $_POST['address'], $_POST['country'], $_POST['phone'], $_POST['email'], $_POST['pwdQn'], $_POST['pwdAns'], $_SESSION["ShopperID"]); 	// "s" - string 
+        $stmt->execute();
+        $stmt->close();
+        $MainContent = "<p style='text-align:center;'>Your profile has been updated!<p/>";
+    }
 }
 
-$MainContent .= "</div>";
 include("MasterTemplate.php"); 
 ?>
